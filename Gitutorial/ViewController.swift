06d10b1b87d5b1e671d9cebd2c8c69c4c937ae03
Julien NIAMKE <67906908+Davekabiyesis@users.noreply.github.com>
2022-09-08 15:06:35 +0000

    v2

--- Gitutorial/ViewController.swift
+++ Gitutorial/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Je comprend maintenant")
+        print("Wai c'est bon deja")
     }
 
 
